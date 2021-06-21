fe5298d0ad0dd6e42d6db4cc7b052c5d03c1ccb1
Deborah Botchwey <deborahbotchwey@gmail.com>
2021-06-20 23:55:41 -0600

    Unit conversion and overview toggle added

---
+++ src/Temperature.js
@@ -0,0 +1,39 @@
+import React, {useState} from "react";
+
+export default function Temperature(props){
+    const [unit, setUnit] = useState("celsius");
+
+    function showFahrenheit(event){
+        event.preventDefault();
+        setUnit("fahrenheit");
+    }
+
+    function showCelsius(event){
+        event.preventDefault();
+        setUnit("celsius");
+    }
+    function fahrenheit(){
+        return (props.celsius * 9/5) + 32;
+    }
+
+    if(unit === "celsius"){
+        return(
+            <h2 className="temp">
+                {Math.round(props.celsius)}
+                <span>
+                    <strong>°C</strong> |<a href="/" onClick={showFahrenheit}>°F</a>
+                </span> 
+            </h2>
+        ); 
+    }else{        
+        return(
+            <h2 className="temp">
+                {Math.round(fahrenheit())}
+                <span>
+                    <a href="/" onClick={showCelsius}>°C</a> |<strong>°F</strong>
+                </span> 
+            </h2>
+        );
+    }
+
+}
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,28 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactAnimatedWeather from 'react-animated-weather';
 import WeatherIcon from "./WeatherIcon";
 import DateTimeFormat from "./DateTimeFormat";
+import Temperature from "./Temperature";
 
 
-export default function weatherInfo(props){
+export default function WeatherInfo(props){
+    const [overviewVisibility, setOverviewVisibility] = useState(false);
+
+    function showOverview(){
+        overviewVisibility ? setOverviewVisibility(false) : setOverviewVisibility(true);
+
+    }
     return(<div className="weatherInfo">
 
-            {/* weather info start */}
             <h3>{props.weather.city}</h3>
 
             <WeatherIcon iconId= {props.weather.iconId} />
 
-            <h2 className="temp">{Math.round(props.weather.temperature)}<span><a href="/">°C</a> |<a href="/">°F</a></span> </h2>
+            <Temperature celsius={props.weather.temperature} />
+
             <ul>                  
                 <li className="description"><h1>{props.weather.description}</h1></li>
                 <li><DateTimeFormat date = {props.weather.date}/></li>           
             </ul>
 
-            {/* 
+            <button onClick={showOverview}>Toggle overview</button>
+
+            {
+            overviewVisibility ?
+                 
             <ul className="row overview">
                 <li className="col">Humidity: 74%</li>
                 <li className="col">Wind: 18 km/h</li>
-            </ul>  */}
+            </ul> : " "  
+            }
            
     </div>);
 }
